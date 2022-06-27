7afe172390efd4a782a96d29880684ec7af20352
MrAsmodeo <108297160+MrAsmodeo@users.noreply.github.com>
2022-06-27 13:43:55 -0500

    Update testeo.py
    1.0

--- testeo.py
+++ testeo.py
@@ -2,4 +2,7 @@ x = float(input('por favor ingresa un valor: '))
 y = float(input('por favor ingresa un valor: '))
 z =  x - y
 z = float(z)
-print('el resultado de la restas es: ',z)
+if z >= 0 :
+    print('tu resultado es ', z)
+else :
+    print('tu valor es negativo ', z)
